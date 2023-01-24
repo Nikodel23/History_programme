503606aa03186da1b860adc3748823815eee6793
Nikodel23 <113675244+Nikodel23@users.noreply.github.com>
2023-01-24 16:13:23 +0100

    gfktyfhtfthf

---
+++ debut_history.py
@@ -0,0 +1,12 @@
+def nom_de_utilisateur_base():
+    nom_de_utilisateur= input("Quelle est le nom de votre héro?")
+    return nom_de_utilisateur
+    
+
+def age_de_utilisateur_base():
+    age_de_utilisateur= input("Quelle age avez vous?"):
+if  age_de_utilisateur<12
+    print("Ce jeu comporte un contenu choquant.")
+        exit()
+    age_de_utilisateur>12
+   pass
