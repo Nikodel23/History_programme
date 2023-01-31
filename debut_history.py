f45dffe123a3a9fbf18b26128d395738daf94042
Nikodel23 <113675244+Nikodel23@users.noreply.github.com>
2023-01-31 16:14:39 +0100

    gg

--- debut_history.py
+++ debut_history.py
@@ -1,4 +1,4 @@
-from data import *
+from data import*
 
 def nom_de_utilisateur_base():
     nom_de_utilisateur= input("Quelle est le nom de votre hero?")
@@ -7,12 +7,13 @@ def nom_de_utilisateur_base():
 def ask_for_age ():
     n = input("Quelle age avez vous ?  ")
     return n
+ 
+age = ask_for_age ()
     
 def user_too_young (n):
     return n in young_ages
 
 def age_de_utilisateur_base():
-    age = ask_for_age ()
     while user_too_young (age):
         age = ask_for_age ()
         pass
