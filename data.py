1dd4814412c2b239062dca3ee89cd35fbb72f5d9
Nikodel23 <113675244+Nikodel23@users.noreply.github.com>
2023-02-11 21:49:11 +0100

    probleme sur l'age

--- data.py
+++ data.py
@@ -4,8 +4,14 @@ Created on Fri Jan 27 10:58:02 2023
 
 @author: delarre
 """
-
-young_ages = [0,1,2,3,4,5,6,7,8,9,10,11,12]
+all_ages =[13,14,15,16,17,18,19,20,21,22,23,24,25,26, \
+ 27,28,29,30,31,32,33,34,35,36,37,38,39,40,41, \
+     42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57, \
+         58,59,60,61,62,63,64,65,66,67,68,69,70,71,72, \
+             73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,\
+                 89,90,91,92,93,94,95,96,97,98,99, \
+                  100,101,102,103,104,105,106,107,108,110, \
+                      111,112,113,114,115,116,117,118,119,120]
 correct_ages = \
     [13,14,15,16,17,18,19,20,21,22,23,24,25,26, \
      27,28,29,30,31,32,33,34,35,36,37,38,39,40,41, \
@@ -22,3 +28,4 @@ text_for_clients = ("Bonjour, vous avez le choix entre ces 3 hisoires")
 intro_texte_history_one = ("Bievenu soldat, tu te trouves en ce moment sur un champ de bataille dans une plaine et vous vous battez contre des allemands. Malheuresement pendant ton sommeil une ogive a explosé dans ton camp. Tu as miraculuseument survecu mais tes coequipier son mort.")
 intro_texte_history_two = ("Bienvenu chers detective, en ce moment tu enquetes sur une mort soudaine d'une victime sous le nom de Miguel .Cette mort est arrivé dans la ville d'acôté. Vous étes le seul detective dans les parages et malgré votre peu d'experience les habitants comte sur vous.") 
 intro_texte_history_three = ("Bienvenu, tu es étudiant dans une école de commerce. Tu as ete accepté et c'est ton premier jour de cours. Tu viens de te lever et tu te prepares pour aller en cours.")
+
--- debut_history.py
+++ debut_history.py
@@ -1,22 +1,27 @@
-from data import*
+from data import (all_ages,correct_ages,text_for_clients,intro_texte_history_one,intro_texte_history_two,intro_texte_history_three)
+
+def verification_erronee():
+    print ("Le nombre que vous avez tapé est incorrect.")
+    return ask_for_age ()
+
+def ask_for_age ():
+        n = input("Quelle age avez vous ?")
+        if n == correct_ages:
+            if age not in all_ages:
+                verification_erronee()
+        else:
+           n = correct_ages
+           return n
+pass
+age = ask_for_age ()
 
 def nom_de_utilisateur_base():
     nom_de_utilisateur= input("Quelle est le nom de votre hero?")
     return nom_de_utilisateur
-    
-def ask_for_age ():
-    n = input("Quelle age avez vous ?  ")
-    return n
- 
-age = ask_for_age ()
-    
-def user_too_young (n):
-    return n in young_ages
+    nom_de_utilisateur = nom_de_utilisateur_base()
 
-def age_de_utilisateur_base():
-    while user_too_young (age):
-            age = ask_for_age ()
-            pass
+
+
     
 def trois_histoires():
     print(text_for_clients)
@@ -32,3 +37,17 @@ def trois_histoires():
             third_history
     if action == 3:
         exit
+        
+        
+def first_history():
+    
+    pass
+    
+    
+def second_history():
+    
+    pass
+    
+def third_history():
+    
+    pass
